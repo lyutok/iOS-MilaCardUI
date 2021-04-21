0d17cb5c440e5115065576d892c48fb4a5003c9a
Mila <mila.liutok@gmail.com>
2021-04-21 11:39:30 +0300

    Initial Commit

---
+++ MilaCardUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MilaCardUI
+//
+//  Created by Lyudmila Tokar on 4/21/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
